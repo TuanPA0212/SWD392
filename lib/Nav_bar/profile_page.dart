5a0bc5ff857d90318c58c2bffd0d5037a6982f89
TuanPA0212 <phananhtuan439@gmail.com>
2023-03-20 12:18:39 +0700

    get student inform

--- lib/Nav_bar/profile_page.dart
+++ lib/Nav_bar/profile_page.dart
@@ -2,11 +2,14 @@ import 'dart:convert';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:swd_project/Nav_bar/qr_scan.dart';
 import 'package:swd_project/common_widget/color.dart';
 import 'package:swd_project/Nav_bar/edit_profile.dart';
 import 'package:swd_project/Nav_bar/notification_page.dart';
+import 'package:swd_project/model/student.dart';
 import 'package:swd_project/widgets/badge.dart';
 import 'package:swd_project/widgets/upload.dart';
 import '../login.dart';
@@ -75,132 +78,7 @@ class _ProfilePageState extends State<ProfilePage> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Center(
-                      child: Column(
-                        children: [
-                          CircleAvatar(
-                            radius: 50.0,
-                            backgroundImage: NetworkImage(
-                              imgState.isNotEmpty
-                                  ? imgState
-                                  : FirebaseAuth
-                                      .instance.currentUser!.photoURL!,
-                            ),
-                          ),
-                          const SizedBox(height: 16.0),
-                          Text(
-                            FirebaseAuth.instance.currentUser!.displayName!,
-                            style: const TextStyle(
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const SizedBox(height: 16.0),
-                              Text(
-                                "Point: ",
-                                style: const TextStyle(
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                "100",
-                                style: const TextStyle(
-                                    fontSize: 18.0,
-                                    fontWeight: FontWeight.bold,
-                                    color: Color.fromARGB(255, 1, 168, 12)),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 32.0),
-                    const Text(
-                      'Student Information',
-                      style: TextStyle(
-                          fontSize: 18.0, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 16.0),
-                    // Upload(),
-
-                    const Text(
-                      'Email',
-                      style: TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                    ),
-                    const SizedBox(height: 8.0),
-                    Text("${FirebaseAuth.instance.currentUser!.email}",
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.grey[600],
-                        )),
-
-                    const SizedBox(height: 16.0),
-                    const Text(
-                      'Address',
-                      style: TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                    ),
-                    const SizedBox(height: 8.0),
-                    Text("${FirebaseAuth.instance.currentUser!.email}",
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.grey[600],
-                        )),
-                    const SizedBox(height: 16.0),
-
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              const Text(
-                                'Birthday',
-                                style: TextStyle(
-                                    fontSize: 16.0,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
-                              ),
-                              const SizedBox(height: 8.0),
-                              Text("22/10/2001",
-                                  style: TextStyle(
-                                    fontSize: 16.0,
-                                    color: Colors.grey[600],
-                                  )),
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              const Text(
-                                'Phone Number',
-                                style: TextStyle(
-                                    fontSize: 16.0,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
-                              ),
-                              const SizedBox(height: 8.0),
-                              Text("09999999",
-                                  style: TextStyle(
-                                    fontSize: 16.0,
-                                    color: Colors.grey[600],
-                                  )),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+                    informationStudent(),
                     // const SizedBox(height: 16.0),
                     // const Text(
                     //   'Point',
@@ -291,5 +169,175 @@ class _ProfilePageState extends State<ProfilePage> {
                 ),
               ),
             )));
+  }
+
+  Widget informationStudent() {
+    return FutureBuilder<Student>(
+      future: fetchStudent(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final student = snapshot.data!;
+          return SingleChildScrollView(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Center(
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 50.0,
+                      backgroundImage: NetworkImage(
+                        imgState.isNotEmpty
+                            ? imgState
+                            : FirebaseAuth.instance.currentUser!.photoURL!,
+                      ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    Text(
+                      // FirebaseAuth.instance.currentUser!.displayName!,
+                      student.name,
+                      style: const TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 16.0),
+                        Text(
+                          "Point: ",
+                          style: const TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "100",
+                          style: const TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(255, 1, 168, 12)),
+                        ),
+                      ],
+                    ),
+                  ], // <-- add closing bracket here
+                ),
+              ),
+              const SizedBox(height: 32.0),
+              const Text(
+                'Student Information',
+                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16.0),
+              // Upload(),
+
+              const Text(
+                'Email',
+                style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+              const SizedBox(height: 8.0),
+              Text(
+                  // "${FirebaseAuth.instance.currentUser!.email}",
+                  student.email,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.grey[600],
+                  )),
+
+              const SizedBox(height: 16.0),
+              const Text(
+                'Address',
+                style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+              const SizedBox(height: 8.0),
+              Text(
+                  // "${FirebaseAuth.instance.currentUser!.email}",
+                  student.address!,
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.grey[600],
+                  )),
+              const SizedBox(height: 16.0),
+
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const Text(
+                          'Birthday',
+                          style: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                        ),
+                        const SizedBox(height: 8.0),
+                        Text(
+                            // "22/10/2001",
+                            DateFormat('dd-MM-yyyy').format(student.birthday),
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.grey[600],
+                            )),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const Text(
+                          'Phone Number',
+                          style: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black),
+                        ),
+                        const SizedBox(height: 8.0),
+                        Text(
+                            // "09999999",
+                            student.phone!,
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.grey[600],
+                            )),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ]),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
+  Future<Student> fetchStudent() async {
+    final storage = new FlutterSecureStorage();
+    final idStudent = int.tryParse(await storage.read(key: 'idStudent') ?? '');
+    final response = await http.get(Uri.parse(
+        "https://event-project.herokuapp.com/api/student/$idStudent"));
+
+    if (response.statusCode == 200) {
+      final responseData = json.decode(response.body) as Map<String, dynamic>;
+      if (responseData.isEmpty) {
+        throw Exception('No student found with id $idStudent');
+      }
+      return Student.fromJson(responseData);
+    } else {
+      throw Exception("Fail to fetch");
+    }
   }
 }
---
+++ lib/model/student.dart
@@ -0,0 +1,40 @@
+class Student {
+  int studentId;
+  int? dpmId;
+  int? campusId;
+  String name;
+  String? address;
+  String? phone;
+  String email;
+  DateTime birthday;
+  String? studentImg;
+  int? point;
+
+  Student({
+    required this.studentId,
+    required this.dpmId,
+    required this.campusId,
+    required this.name,
+    required this.address,
+    required this.phone,
+    required this.email,
+    required this.birthday,
+    this.studentImg,
+    this.point,
+  });
+
+  factory Student.fromJson(Map<String, dynamic> json) => Student(
+        studentId: json["student_id"],
+        dpmId: json["dpm_id"],
+        campusId: json["campus_id"],
+        name: json["name"],
+        address: json["address"],
+        phone: json["phone"],
+        email: json["email"],
+        birthday: json['birthday'] != null
+            ? DateTime.parse(json['birthday'])
+            : DateTime.now(),
+        studentImg: json["student_img"],
+        point: json["point"],
+      );
+}
