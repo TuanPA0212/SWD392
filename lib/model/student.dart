3c09507f0cf8b02bc7c22ce0d6d422a6d2a26744
TuanPA0212 <phananhtuan439@gmail.com>
2023-03-23 22:49:55 +0700

    fix point

--- lib/model/student.dart
+++ lib/model/student.dart
@@ -35,6 +35,6 @@ class Student {
             ? DateTime.parse(json['birthday'])
             : DateTime.now(),
         studentImg: json["student_img"],
-        point: json["point"],
+        point: int.tryParse(json["point"]) ?? 0,
       );
 }
